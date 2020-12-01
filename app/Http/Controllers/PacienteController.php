ea673f0a4baa5e975eeb08c380109ce31e088d2b
Diego Mendes <gdmendes55@gmail.com>
2020-12-01 13:07:38 -0400

    primeiro comiit

---
+++ app/Http/Controllers/PacienteController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Paciente;
+use Illuminate\Http\Request;
+
+class PacienteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $pacientes = Paciente::all();
+
+        return view('pacientes.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $paciente = Paciente::find($id);
+        return view('pacientes.edit', compact('paciente'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+
+        $request->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required'
+        ]);
+
+        $paciente = Paciente::find($id);
+        $paciente->first_name =  $request->get('first_name');
+        $paciente->last_name = $request->get('last_name');
+        $paciente->email = $request->get('email');
+        $paciente->job_title = $request->get('job_title');
+        $paciente->city = $request->get('city');
+        $paciente->country = $request->get('country');
+        $paciente->save();
+
+        return redirect('/contacts')->with('success', 'Paciente Atualizado!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $paciente = Paciente::find($id);
+        $paciente->delete();
+
+        return redirect('/pacientes')->with('success', 'Paciente deletado!');
+    }
+}
