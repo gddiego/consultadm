9d21e1f086313dfeaa0ce094dfa271f7b63ff2fc
Diego Mendes <gdmendes55@gmail.com>
2020-12-02 15:40:16 -0400

    agendamentos, pacientes and medicos

---
+++ resources/views/medicos/index.blade.php
@@ -0,0 +1,54 @@
+@extends('base')
+<div class="col-sm-12">
+
+    @if(session()->get('success'))
+      <div class="alert alert-success">
+        {{ session()->get('success') }}
+      </div>
+    @endif
+  </div>
+@section('main')
+<div>
+    <a  type="button" class="btn btn-primary"  href="{{ route('pacientes.create')}}" >Novo</a>
+    </div>
+<div class="row">
+<div class="col-sm-12">
+    <h1 class="display-3">Pacientes</h1>
+  <table class="table table-striped">
+    <thead>
+        <tr>
+          <td>ID</td>
+          <td>Name</td>
+          <td>Email</td>
+          <td>Trabalho</td>
+          <td>Cidade</td>
+          <td>Enderço</td>
+          <td colspan = 2>Actions</td>
+        </tr>
+    </thead>
+    {{-- <tbody>
+        @foreach($contacts as $contact)
+        <tr>
+            <td>{{$contact->id}}</td>
+            <td>{{$contact->first_name}} {{$contact->last_name}}</td>
+            <td>{{$contact->email}}</td>
+            <td>{{$contact->job_title}}</td>
+            <td>{{$contact->city}}</td>
+            <td>{{$contact->country}}</td>
+            <td>
+                <a href="{{ route('contacts.edit',$contact->id)}}" class="btn btn-primary">Edit</a>
+            </td>
+            <td>
+                <form action="{{ route('contacts.destroy', $contact->id)}}" method="post">
+                  @csrf
+                  @method('DELETE')
+                  <button class="btn btn-danger" type="submit">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+  </table> --}}
+<div>
+</div>
+@endsection
